4bb8186efb75a001b9a1ca5a5a492f940864e9b9
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2025-09-03 20:34:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.cpp
@@ -0,0 +1,38 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val) {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *reverse(Node *head) {
+        if (!head) return NULL;
+
+        Node* curr = head;
+        Node* temp = NULL;
+
+       
+        while (curr != NULL) {
+            temp = curr->prev;
+            curr->prev = curr->next;
+            curr->next = temp;
+            curr = curr->prev; 
+        }
+
+       
+        if (temp != NULL) {
+            head = temp->prev;
+        }
+
+        return head;
+    }
+};

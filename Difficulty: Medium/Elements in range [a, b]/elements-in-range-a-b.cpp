5d6d040b21ccf8d5c64972b7dc7c724056ef40ce
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2025-12-23 23:13:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Elements in range [a, b]/elements-in-range-a-b.cpp
+++ Difficulty: Medium/Elements in range [a, b]/elements-in-range-a-b.cpp
@@ -1,13 +1,25 @@
 class Solution {
   public:
     vector<int> cntInRange(vector<int> &arr, vector<vector<int>> &queries) {
+     
         sort(arr.begin(), arr.end());
-        vector<int> ans;
-        for (auto& q : queries) {
-            auto low = lower_bound(arr.begin(), arr.end(), q[0]);
-            auto high = upper_bound(arr.begin(), arr.end(), q[1]);
-            ans.push_back(high - low);
+        
+        vector<int> results;
+        
+        
+        for (const auto& query : queries) {
+            int lower = query[0];
+            int upper = query[1];
+            
+            auto it1 = lower_bound(arr.begin(), arr.end(), lower);
+            
+       
+            auto it2 = upper_bound(arr.begin(), arr.end(), upper);
+            
+          
+            results.push_back(it2 - it1);
         }
-        return ans;
+        
+        return results;
     }
 };

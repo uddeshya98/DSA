dc3ff3945149d8cceffe8d8d061d3b134753ea8f
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-01-02 09:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (69.74%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int x : nums) {
+            if (s.count(x)) return x;
+            s.insert(x);
+        }
+        return -1;
+    }
+};

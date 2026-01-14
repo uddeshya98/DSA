c4db9ebb9c61463fb19f51ba5f835c769f97515c
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-01-14 11:33:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int catchThieves(vector<char> &arr, int k) {
+        vector<int> police, thief;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == 'P') police.push_back(i);
+            else thief.push_back(i);
+        }
+        
+        int i = 0, j = 0, ans = 0;
+        
+        while (i < police.size() && j < thief.size()) {
+            if (abs(police[i] - thief[j]) <= k) {
+                ans++;
+                i++;
+                j++;
+            }
+            else if (police[i] < thief[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};

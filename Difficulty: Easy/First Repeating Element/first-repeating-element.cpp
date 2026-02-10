e66670f6226ca77a51f7e9c4f36c72f1c3b30459
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-02-11 01:00:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstRepeated(vector<int> &arr) {
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(arr[i] == arr[j]){
+                    return i + 1;   
+                }
+            }
+        }
+        return -1;
+    }
+};

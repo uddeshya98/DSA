a9d432d7dd1824149fd4bb477f5839bf154722f0
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-02-11 01:23:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
@@ -1,15 +1,16 @@
 class Solution {
-public:
+  public:
     int firstRepeated(vector<int> &arr) {
-        int n = arr.size();
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(arr[i] == arr[j]){
-                    return i + 1;   
-                }
-            }
+    unordered_map<int, int>hash;
+    int n = arr.size();
+    for(int i = 0; i<n; i++){
+        hash[arr[i]]++;
+    }
+    for(int i = 0; i <n; i++){
+        if(hash[arr[i]] > 1){
+            return i+1;
         }
+    }
         return -1;
     }
 };

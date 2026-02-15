f7f77a513313924b5490a35ac8513ccbb5e51e1f
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-02-15 17:51:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (80.99%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i = a.size() - 1;
-        int j = b.size() - 1;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
-        string result = "";
+        string ans = "";
 
         while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
 
-            if (i >= 0) sum += a[i--] - '0';
-            if (j >= 0) sum += b[j--] - '0';
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
 
-            result.push_back((sum % 2) + '0');
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            ans += char((sum % 2) + '0');
             carry = sum / 2;
         }
 
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };

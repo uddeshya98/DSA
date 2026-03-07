9da0714315f39875ec85fa2db507a400b84be1c9
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-03-07 23:43:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dice throw/dice-throw.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int noOfWays(int m, int n, int x) {
+        vector<vector<long long>> dp(n + 1, vector<long long>(x + 1, 0));
+
+        dp[0][0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= x; j++){
+                for(int k = 1; k <= m && k <= j; k++){
+                    dp[i][j] += dp[i-1][j-k];
+                }
+            }
+        }
+
+        return dp[n][x];
+    }
+};

f1883fedfe38c2d810307e78722947d451a33403
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-04-06 16:33:56 +0530

    Time: 25 ms (55.73%) | Memory: 37.7 MB (66.40%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        set<pair<int,int>> obs;
+        for (auto &o : obstacles) obs.insert({o[0], o[1]});
+
+        vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
+        int d = 0, x = 0, y = 0, ans = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                d = (d + 1) % 4;
+            } else if (cmd == -2) {
+                d = (d + 3) % 4;
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dir[d].first;
+                    int ny = y + dir[d].second;
+                    if (obs.count({nx, ny})) break;
+                    x = nx;
+                    y = ny;
+                    ans = max(ans, x*x + y*y);
+                }
+            }
+        }
+        return ans;
+    }
+};

7accb834c6fcdc3085b3442bd24656da2a66f5c9
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-05-05 23:21:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of XOR of all pairs/sum-of-xor-of-all-pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long sumXOR(vector<int> arr) {
+        int n = arr.size();
+        long long ans = 0;
+
+        for(int bit = 0; bit < 32; bit++) {
+            long long count1 = 0;
+
+            for(int i = 0; i < n; i++) {
+                if(arr[i] & (1 << bit)) count1++;
+            }
+
+            long long count0 = n - count1;
+            ans += count1 * count0 * (1LL << bit);
+        }
+
+        return ans;
+    }
+};

6a6884463b22d0c6e59ec95f18f24f8955236533
uddeshya98 <158562852+uddeshya98@users.noreply.github.com>
2026-05-12 15:38:40 +0530

    Time: 31 ms (83.98%) | Memory: 97.4 MB (39.81%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](const vector<int>& a, const vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+        
+        int ans = 0;
+        int curr = 0;
+        
+        for (const auto& task : tasks) {
+            if (curr < task[1]) {
+                ans += (task[1] - curr);
+                curr = task[1];
+            }
+            curr -= task[0];
+        }
+        
+        return ans;
+    }
+};
